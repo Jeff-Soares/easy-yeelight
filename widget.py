21c01a129d90d9b3296ecdb3b0b39c4a723ba0e6
Jeff Soares <jeffersonvsoares@hotmail.com>
2020-03-21 15:11:51 -0300

    Changed Tk geometry management to Grid

--- widget.py
+++ widget.py
@@ -1,29 +1,42 @@
-from tkinter import *
-from yeelight import Bulb
+from tkinter import Tk, PhotoImage, Label, Button
+from PIL import ImageTk, Image
+from yeelight import Bulb, discover_bulbs
 
 main = Tk()
 main.title("Easy Yeelight")
-main.geometry("200x300+200+100")
+main.geometry("300x400+400+300")
+main.resizable(False, False)
 
-Bulb = Bulb("192.168.15.6")
-imgOff = PhotoImage(file='img/bulb_off.png')
-imgOn = PhotoImage(file='img/bulb_on.png')
+# Using resize method while the size is not defined.
+imgOff = ImageTk.PhotoImage(Image.open(
+    'img/bulb_off.png').resize((60, 120), Image.ANTIALIAS))
+imgOn = ImageTk.PhotoImage(Image.open(
+    'img/bulb_on.png').resize((60, 120), Image.ANTIALIAS))
+
+# Standard value, will be changed to "" later
+bulb = Bulb("192.168.15.2")
+
+
+def verifyState(b):
+    return True if b.get_properties()["power"] == 'on' else False
+
 
 def bulb_on():
-    Bulb.turn_on()
+    bulb.turn_on()
     bulbImg.configure(image=imgOn)
 
+
 def bulb_off():
-    Bulb.turn_off()
+    bulb.turn_off()
     bulbImg.configure(image=imgOff)
 
-bulbImg = Label(main, image=imgOff)
-bulbImg.pack(side="top", padx=10, pady=10)
 
-on = Button(main, text="ON", width=10, command=bulb_on)
-on.pack(side="left", padx=10, pady=10)
+bulbImg = Label(main, image=imgOn if verifyState(bulb) else imgOff)
+on = Button(main, text="ON", width=8, command=bulb_on)
+off = Button(main, text="OFF", width=8, command=bulb_off)
 
-off = Button(main, text="OFF", width=10, command=bulb_off)
-off.pack(side="right", padx=10, pady=10)
+bulbImg.grid(column=0, row=0, columnspan=2, pady=5, padx=5)
+on.grid(column=0, row=1)
+off.grid(column=1, row=1)
 
 main.mainloop()
