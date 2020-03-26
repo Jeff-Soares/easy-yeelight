0f3c87fa3c9185db53c6d656243848b89af62828
Jeff Soares <jeffersonvsoares@hotmail.com>
2020-03-26 20:25:41 -0300

    Add brightness and RGB control widgets

--- widget.py
+++ widget.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Frame, Label, Button, Listbox, Radiobutton, messagebox, simpledialog, END, StringVar
+from tkinter import Tk, Frame, LabelFrame, Label, Button, Listbox, Radiobutton, Scale, messagebox, simpledialog, END
 from tkinter.ttk import Separator
 from PIL import ImageTk, Image
 from yeelight import Bulb, discover_bulbs
@@ -25,6 +25,8 @@ imgOff = ImageTk.PhotoImage(Image.open(
     'img/bulb_off.png').resize((60, 120), Image.ANTIALIAS))
 imgOn = ImageTk.PhotoImage(Image.open(
     'img/bulb_on.png').resize((60, 120), Image.ANTIALIAS))
+imgRgb = ImageTk.PhotoImage(Image.open(
+    'img/rgb_scale.png').resize((250, 10), Image.ANTIALIAS))
 
 
 def discoverIp():
@@ -128,6 +130,8 @@ def activateBulb(b):
     refreshImgState(b)
     on.configure(command=lambda: bulb_on(b), state="active")
     off.configure(command=lambda: bulb_off(b), state="active")
+    ip_label.configure(text=b._ip)
+    model_label.configure(text=re.sub(r"BulbType\.", "", str(b.bulb_type)))
 
 
 def refreshImgState(b):
@@ -158,9 +162,22 @@ sep = Separator(search_frame, orient="horizontal")
 devices_title = Label(search_frame, bg=bg_color,
                       text="DEVICES", font=('Sans-serif', '15', 'bold'))
 
+info_lframe = LabelFrame(control_frame, text="Details", width=280, height=35)
+ip_info_label = Label(info_lframe, text="IP Address:")
+ip_label = Label(info_lframe, text="")
+model_info_label = Label(info_lframe, text="Model:")
+model_label = Label(info_lframe, text="")
 bulbImg = Label(control_frame, image=imgOff)
-on = Button(control_frame, text="ON", width=8, state="disable")
-off = Button(control_frame, text="OFF", width=8, state="disable")
+on = Button(control_frame, text="ON", width=6, state="disable")
+off = Button(control_frame, text="OFF", width=6, state="disable")
+delete = Button(control_frame, text="Delete", width=6, state="disable")
+brightness = Scale(control_frame, label="Brightness",
+                   orient="horizontal", length=250, from_=1.0, to=100)
+temp = Scale(control_frame, label="Temp", orient="horizontal",
+             length=250, from_=1700, to=6500)
+color = Scale(control_frame, label="Color",
+              orient="horizontal", length=250, showvalue=0)
+rgb_scale = Label(control_frame, image=imgRgb)
 
 search_frame.grid(column=0, row=0, sticky="e")
 search_frame.grid_propagate(0)
@@ -175,11 +192,26 @@ search_button.grid(column=0, row=0, pady=10)
 add_button.grid(column=1, row=0, pady=10)
 ip_list.grid(column=0, row=1, columnspan=2, padx=5)
 sep.grid(column=0, row=2, columnspan=2, pady=10, padx=10, sticky="ew")
+
 devices_title.grid(column=0, row=3, columnspan=2)
 
-bulbImg.grid(column=0, row=0, columnspan=2, pady=5, padx=5)
-on.grid(column=0, row=1)
-off.grid(column=1, row=1)
+info_lframe.grid(column=0, row=0, columnspan=4,
+                 pady=5, padx=5, ipady=5, ipadx=5)
+info_lframe.grid_propagate(0)
+info_lframe.columnconfigure(1, minsize=100)
+info_lframe.columnconfigure(3, minsize=50)
+ip_info_label.grid(column=0, row=0)
+ip_label.grid(column=1, row=0)
+model_info_label.grid(column=2, row=0)
+model_label.grid(column=3, row=0)
+bulbImg.grid(column=0, row=1, rowspan=3, pady=5, padx=5)
+on.grid(column=1, row=1)
+off.grid(column=1, row=2)
+delete.grid(column=1, row=3)
+brightness.grid(column=0, row=4, columnspan=4)
+temp.grid(column=0, row=5, columnspan=4)
+color.grid(column=0, row=6, columnspan=4)
+rgb_scale.grid(column=0, row=7, columnspan=4)
 
 saveBulbs()
 main.mainloop()
